6501579af5921e411b5cb6785bb9776fd6596c5e
Muhammad Awais Akhtar <muhammad.a.akhtar@student.fh-kiel.de>
2015-04-24 00:53:08 +0200

    k2Bridge Up: kSquareBridge

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # k2Bridge
+
+
+$> thrift.exe --gen java hands.thrift
+$> thrift.exe --gen csharp hands.thrift
---
+++ hands.thrift
@@ -0,0 +1,31 @@
+#GPL2
+#K2Bridge
+#Awais (creative technologies FH)
+
+
+enum HandState {
+	CLOSED=0,
+	OPEN=1,
+	LASSO=2
+}
+
+enum TrackingState{
+	Inferred=0,
+	Tracked=1
+}
+
+struct Hand{
+	1: double x,
+	2: double y,
+	3: HandState handState,
+	4:TrackingState trackingState
+}
+
+struct Hands {
+	1:Hand left,
+	2:Hand right
+}
+
+service HandService{
+	Hands getHands()
+}
