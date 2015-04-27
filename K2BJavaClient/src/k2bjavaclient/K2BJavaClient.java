c893954c736ce663db6880526eb25d64cbbae8d0
Muhammad Awais Akhtar <muhammad.a.akhtar@student.fh-kiel.de>
2015-04-27 17:38:16 +0200

    k2BLib.jar for using in projects

--- K2BJavaClient/src/k2bjavaclient/K2BJavaClient.java
+++ K2BJavaClient/src/k2bjavaclient/K2BJavaClient.java
@@ -6,17 +6,13 @@
 
 package k2bjavaclient;
 
+import static java.lang.Thread.sleep;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import k2Bridge.HandService;
 import k2Bridge.Hands;
+import k2bConnect.k2bClient;
 import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TSSLTransportFactory;
-import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
 
 /**
  *
@@ -29,61 +25,32 @@ public class K2BJavaClient {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
+        
+        k2bClient client = new k2bClient();
         try {
-            TTransport transport;
-            if (true) {
-                transport = new TSocket("localhost", 9090);
-                transport.open();
-            } else {
-                /*
-                 * Similar to the server, you can use the parameters to setup client parameters or
-                 * use the default settings. On the client side, you will need a TrustStore which
-                 * contains the trusted certificate along with the public key. 
-                 * For this example it's a self-signed cert. 
-                 */
-                TSSLTransportParameters params = new TSSLTransportParameters();
-                params.setTrustStore("../../lib/java/test/.truststore", "thrift", "SunX509", "JKS");
-                /*
-                 * Get a client transport instead of a server transport. The connection is opened on
-                 * invocation of the factory method, no need to specifically call open()
-                 */
-                transport = TSSLTransportFactory.getClientSocket("localhost", 9091, 0, params);
+            client.connect("localhost", 9090);
+            
+            while(true){
+                Hands hands= client.getHands();
+                System.out.println("leftX:"+hands.left.x);
+                System.out.println("leftY:"+hands.left.y);
+                System.out.println("state:"+hands.left.handState);
+                
+                System.out.println("rightX:"+hands.right.x);
+                System.out.println("rightY:"+hands.right.y);
+                System.out.println("state:"+hands.right.handState);
+                
+                sleep(5000); //5sec
             }
-
-            TProtocol protocol = new TBinaryProtocol(transport);
-            HandService.Client client = new HandService.Client(protocol);
-
-            perform(client);
-
-            transport.close();
-        } catch (TException x) {
-            x.printStackTrace();
-        }
-    }
-
-    private static void perform(HandService.Client client) throws TException {
-
-        try {
-            while (true) {
-                Hands hand = client.getHands();
-                System.out.println("leftX:"+hand.left.x);
-                System.out.println("leftY:"+hand.left.y);
-                System.out.println("state:"+hand.left.handState);
-                
-                System.out.println("rightX:"+hand.right.x);
-                System.out.println("rightY:"+hand.right.y);
-                System.out.println("state:"+hand.right.handState);
-                
-                
-                Thread.sleep(5000); //five second
-            }
-
+            
+        } catch (TTransportException ex) {
+            Logger.getLogger(K2BJavaClient.class.getName()).log(Level.SEVERE, null, ex);
         } catch (TException ex) {
-            System.out.println("Invalid operation: " + ex.getMessage());
+            Logger.getLogger(K2BJavaClient.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InterruptedException ex) {
             Logger.getLogger(K2BJavaClient.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
     }
 
 }
---
+++ K2BLib/src/k2bConnect/k2bClient.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package k2bConnect;
+
+import k2Bridge.HandService;
+import k2Bridge.Hands;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSSLTransportFactory;
+import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
+/**
+ *
+ * @author Awais Akhtar
+ */
+public class k2bClient {
+    
+    private TTransport transport;
+    private TProtocol protocol;
+    private  HandService.Client client;
+
+    
+    public void connect(String host, int port) throws TTransportException{
+        //simple connection, ignore ssl for now
+        if (true) {
+                transport = new TSocket(host, port); 
+                transport.open();
+            } else {
+                /*
+                 * Similar to the server, you can use the parameters to setup client parameters or
+                 * use the default settings. On the client side, you will need a TrustStore which
+                 * contains the trusted certificate along with the public key. 
+                 * For this example it's a self-signed cert. 
+                 */
+                TSSLTransportParameters params = new TSSLTransportParameters();
+                params.setTrustStore("../../lib/java/test/.truststore", "thrift", "SunX509", "JKS");
+                /*
+                 * Get a client transport instead of a server transport. The connection is opened on
+                 * invocation of the factory method, no need to specifically call open()
+                 */
+                transport = TSSLTransportFactory.getClientSocket(host, port, 0, params);
+            }
+        
+        TProtocol protocol = new TBinaryProtocol(transport);
+        client = new HandService.Client(protocol);        
+    }
+    
+    public Hands getHands() throws TException{
+        return client.getHands();
+    }
+    
+}
