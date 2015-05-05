05b2d59484b4f0dd58c38b845eec91150b270470
Awais <Awaisakhtar16@yahoo.com>
2015-05-05 14:53:26 +0200

    disconnect method

--- K2BLib/src/k2bConnect/k2bClient.java
+++ K2BLib/src/k2bConnect/k2bClient.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package k2bConnect;
 
 import k2Bridge.HandService;
@@ -22,39 +21,44 @@ import org.apache.thrift.transport.TTransportException;
  * @author Awais Akhtar
  */
 public class k2bClient {
-    
+
     private TTransport transport;
     private TProtocol protocol;
-    private  HandService.Client client;
+    private HandService.Client client;
 
-    
-    public void connect(String host, int port) throws TTransportException{
+    public void connect(String host, int port) throws TTransportException {
         //simple connection, ignore ssl for now
         if (true) {
-                transport = new TSocket(host, port); 
-                transport.open();
-            } else {
-                /*
-                 * Similar to the server, you can use the parameters to setup client parameters or
-                 * use the default settings. On the client side, you will need a TrustStore which
-                 * contains the trusted certificate along with the public key. 
-                 * For this example it's a self-signed cert. 
-                 */
-                TSSLTransportParameters params = new TSSLTransportParameters();
-                params.setTrustStore("../../lib/java/test/.truststore", "thrift", "SunX509", "JKS");
-                /*
-                 * Get a client transport instead of a server transport. The connection is opened on
-                 * invocation of the factory method, no need to specifically call open()
-                 */
-                transport = TSSLTransportFactory.getClientSocket(host, port, 0, params);
-            }
-        
+            transport = new TSocket(host, port);
+            transport.open();
+        } else {
+            /*
+             * Similar to the server, you can use the parameters to setup client parameters or
+             * use the default settings. On the client side, you will need a TrustStore which
+             * contains the trusted certificate along with the public key. 
+             * For this example it's a self-signed cert. 
+             */
+            TSSLTransportParameters params = new TSSLTransportParameters();
+            params.setTrustStore("../../lib/java/test/.truststore", "thrift", "SunX509", "JKS");
+            /*
+             * Get a client transport instead of a server transport. The connection is opened on
+             * invocation of the factory method, no need to specifically call open()
+             */
+            transport = TSSLTransportFactory.getClientSocket(host, port, 0, params);
+        }
+
         TProtocol protocol = new TBinaryProtocol(transport);
-        client = new HandService.Client(protocol);        
+        client = new HandService.Client(protocol);
     }
-    
-    public Hands getHands() throws TException{
+
+    public Hands getHands() throws TException {
         return client.getHands();
     }
-    
+
+    public void disconnect() { //
+        if (transport != null) {
+            transport.close();
+        }
+    }
+
 }
